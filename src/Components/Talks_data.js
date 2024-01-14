98a1983f247534d450e47b6fdf35847f7227632b
Asmit Ashutosh <asmit1655@gmail.com>
2024-01-15 00:34:18 +0530

    new talks add

--- src/Components/Talks_data.js
+++ src/Components/Talks_data.js
@@ -17,18 +17,24 @@ export  const Talks_data=[
         ],
         url : 'https://docs.google.com/forms/u/0/d/1WgckB5c2VZ9g9HyYWQ0tBHa0MkNk8txE1ufpOVowDdY/viewform?edit_requested=true'
     },
-    // {
-    //     id:2,
-    //     name:"Code Compendium",
-    //     fullname:"Code-fest - Code Compendium",
-    //     img:"/images/img-1.png",
-    //     isimg: true,
-    //     theme:"",
-    //     description:"'The only way to do great work is to love what you do.' Display your problem-solving prowess. Tread through the branches of algorithms into the world of loops and queues and partake in the exhilarating coding competition, CODE COMPENDIUM . This event offers a remarkable opportunity for you to showcase your coding prowess and bask in the limelight of your brilliance. As we open the gates, the virtual realm awaits your keystrokes, challenging your coding expertise like never before. The EL & TC Society invites you to be part of our technical fest TECHTRONIX. Mark your calendar for this exciting and rousing event, Code Compendium. Let your fingers tap to victory.",
-    //     rules:["Participation for the event will be only accepted for groups. each group must contain 2 members.","Round 1 will have 6 questions.","Out of 6 questions, 3 questions will be using matlab and 3 questions using vivado.","Round 2 details will be disclosed at the event","No use of phone Or internet","Bring your own laptops"
-    //     ],
-    //     url : 'https://docs.google.com/forms/u/0/d/1WgckB5c2VZ9g9HyYWQ0tBHa0MkNk8txE1ufpOVowDdY/viewform?edit_requested=true'
-    // },
+    {
+        id:2,
+        name:"Drone-Venture Summit",
+        fullname:"Talk on Drone-Venture",
+        img:"/images/STARTUP.png",
+        isimg: true,
+        istheme: false,
+        isurl2: false,
+        isRule:false,
+        isprob: false,
+        isTalk:true,
+        theme:"",
+        description:`In the dynamic landscape of the technology industry, drone technology has emerged as a disruptive force with vast potential for innovation and economic growth. As entrepreneurs and investors flock to capitalize on the drone industry's promising future, the Drone-Venture Summit stands out as a crucial platform for charting the source of start-up success in this burgeoning sector.
+        The Drone-Venture Summit serves as a melting pot for pioneers, innovators, and investors looking to navigate the ever-evolving drone industry. The summit creates an environment where ideas take flight, fostering collaboration, and showcasing groundbreaking concepts that have the potential to reshape industries.`,
+        rules:["Participation for the event will be only accepted for groups. each group must contain 2 members.","Round 1 will have 6 questions.","Out of 6 questions, 3 questions will be using matlab and 3 questions using vivado.","Round 2 details will be disclosed at the event","No use of phone Or internet","Bring your own laptops"
+        ],
+        url : 'https://docs.google.com/forms/u/0/d/1WgckB5c2VZ9g9HyYWQ0tBHa0MkNk8txE1ufpOVowDdY/viewform?edit_requested=true'
+    },
     // {
     //     id:3,
     //     name:"LOGIC LABYRINTH",
