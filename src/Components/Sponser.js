8e0f6c2d0655af6af32d507d6137a855435160ce
adarsh3149 <adarshsohamacharya888@gmail.com>
2024-01-16 09:01:59 +0530

    sponsers added

--- src/Components/Sponser.js
+++ src/Components/Sponser.js
@@ -4,7 +4,7 @@ import { Box } from '@mui/material';
 import styled, { keyframes, css } from "styled-components";
 
 const Sponser = () => {
-  
+
   const row1 = [
     "/images/MCL_prev_ui.png",
     "/images/jsw-group-vector-logo_prev_ui.png",
@@ -20,12 +20,12 @@ const Sponser = () => {
     "/images/IEEE power electronics_prev_ui.png",
   ];
   const row3 = [
-    "/images/ESS.jpeg",
+    "/images/ESS_prev_ui.png",
     "/images/Dhaba.jpg"
   ]
   const row4 = [
     "/images/IEEE vssut_prev_ui.png",
-    
+
   ];
   return (
     <>
@@ -103,7 +103,7 @@ const Sponser = () => {
 
 
       </Box>
-     
+
       <Box
         sx={{
           fontSize: "2rem",
